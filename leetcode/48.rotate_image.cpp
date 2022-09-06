b1bd2d5283f9edf3846e8b028f36821276263418
oplibs <oplibs@gmail.com>
2022-09-06 12:13:52 +0800

    mod: 48

--- leetcode/48.rotate_image.cpp
+++ leetcode/48.rotate_image.cpp
@@ -1,33 +1,27 @@
 class Solution {
-public:
-    void rotate(vector<vector<int>>& matrix) {
-      size_t width=matrix.size();
-      if(width == 1){
-        return;
-      };
-      int temp;
-      size_t i = 0;
-      size_t j = 0;
-      //  Try this for starting loop from i+1
-      for(i = 0; i < width; i++){
-        for(j = i+1; j < width ; j++){
-          temp = matrix[i][j];
-          matrix[i][j]=matrix[j][i];
-          matrix[j][i] = temp;
-        }
+  public:
+  void rotate(vector<vector<int>>& matrix) {
+    int n = matrix.size();
+    // Transpose
+    for (int i = 0; i < n; i++) {
+      // Transpose : j<i
+      for (int j = 0; j < i; j++) {
+				if (i != j) {
+				  int temp = matrix[i][j];
+				  matrix[i][j] = matrix[j][i];
+				  matrix[j][i] = temp;
+				}
       }
-      for(i = 0; i < width; i++){
-        for(j = 0; j < width/2 ; j++){
-          temp = matrix[i][j];
-          matrix[i][j]=matrix[i][width-1-j];
-          matrix[i][width-1-j] = temp;
-        }
-      }
-      // for(i = 0; i < width; i++){
-        // for(j = 0; j < width ; j++){
-          // printf("%d,",matrix[i][j]);
-        // }
-        // printf("\n");
-      // }
     }
+    // Mirror
+    for (int i = 0; i < n; i++) {
+      // Mirror : j<n/2
+      for (int j = 0; j < n / 2; j++) {
+				int temp = matrix[i][j];
+				matrix[i][j] = matrix[i][n - 1 - j];
+				matrix[i][n - 1 - j] = temp;
+      }
+    }
+    return;
+  }
 };
