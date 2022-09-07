3098c5f35df0bd5fb3ac16da5d9cfc9589e0e864
oplibs <oplibs@gmail.com>
2022-09-07 20:37:06 +0800

    Mod: better change

--- leetcode/77.combinations.cpp
+++ leetcode/77.combinations.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
-    void iterate(vector<vector<int>>& results, vector<int>& item, int cur, int n, int k){
-      if(item.size()==k){
-        results.push_back(item);
-        return;
-      }
-      for(int i = cur; i<=n; i++){
-        item.push_back(i);
-        // printf("%d-%d\n", i, item.size());
-        iterate(results, item, i+1, n, k);
+    void dfs(vector<vector<int>>& results, vector<int>& item, vector<bool>& visited, int start, int n, int k){
+        if(item.size()==k){
+            results.push_back(item);
+            return;
+        }
+        //如果不是最少两个元素构成子集，可以在start>0完成push，就包含所有自己的情况了
+        if(start>n){
+            return;
+        }
+        item.push_back(start);
+        dfs(results, item, visited, start+1, n, k);
         item.pop_back();
-        // printf("%d-%d\n", i, item.size());
-        // WTF : why add the following line
-        // iterate(results, item, i+1, n, k);
-      }
+        dfs(results, item, visited, start+1, n, k);
     }
-
     vector<vector<int>> combine(int n, int k) {
-      vector<vector<int>> results;
-      vector<int> item;
-      iterate(results, item, 1, n, k);
-      return results;
+        vector<int> item;
+        vector<vector<int>> results;
+        vector<bool> visited(n+1,false);
+        dfs(results, item, visited, 1, n, k);
+        return results;
     }
 };
